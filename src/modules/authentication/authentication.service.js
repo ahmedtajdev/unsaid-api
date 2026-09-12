f99c44cf0014ad2054a3261ca38b3cb83d462a51
ahmedtajdev <ahmedtaj.dev@gmail.com>
2026-09-12 23:33:46 +0300

    signup and login

--- src/modules/authentication/authentication.service.js
+++ src/modules/authentication/authentication.service.js
@@ -1,9 +1,39 @@
-export const signup = (inputs) => {
-  // throw new Error("Email exists", {cause: {status: 409}})
-  return [];
+import { UserModel } from "../../DB/models/index.js";
+import {
+  ConflictException,
+  NotfoundException,
+} from "../../common/exceptions/index.js";
+import { createOne, findOne } from "../../common/repository/index.js";
+
+export const signup = async ({ email, password, username }) => {
+  const duplicatedAccount = await findOne({
+    model: UserModel,
+    filter: { email },
+    options: { select: "email" },
+  });
+
+  if (duplicatedAccount) {
+    throw ConflictException("Email Exists");
+  }
+
+  const account = await createOne({
+    model: UserModel,
+    data: { email, password, username },
+  });
+
+  return account;
 };
 
-export const login = (inputs) => {
-  // throw new Error("Not exists", {cause: {status: 404}})
-  return [];
+export const login = async ({ email, password }) => {
+  const account = await findOne({
+    model: UserModel,
+    filter: { email, password },
+    options: { select: "-password" },
+  });
+
+  if (!account) {
+    throw NotfoundException("Not Exist");
+  }
+
+  return account;
 };
