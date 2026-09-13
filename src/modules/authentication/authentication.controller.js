73d4fddeab87845dc62609a42d31ecb9323ed729
ahmedtajdev <ahmedtaj.dev@gmail.com>
2026-09-13 21:16:41 +0300

    signup, login, postman docs

--- src/modules/authentication/authentication.controller.js
+++ src/modules/authentication/authentication.controller.js
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import { successResponse } from "../../common/utils/index.js";
-import { signup } from "./authentication.service.js";
+import { login, signup } from "./authentication.service.js";
 const router = Router();
 
 router.post("/signup", async (req, res, next) => {
-  const data = signup(req.body);
+  const data = await signup(req.body);
   return successResponse({ res, status: 201, data });
 });
 
-router.post("/login", (req, res, next) => {
-  const data = signup(req.body);
+router.post("/login", async (req, res, next) => {
+  const data = await login(req.body);
   return successResponse({ res, data });
 });
 
--- src/modules/authentication/authentication.service.js
+++ src/modules/authentication/authentication.service.js
@@ -5,35 +5,49 @@ import {
 } from "../../common/exceptions/index.js";
 import { createOne, findOne } from "../../common/repository/index.js";
 
-export const signup = async ({ email, password, username }) => {
-  const duplicatedAccount = await findOne({
-    model: UserModel,
-    filter: { email },
-    options: { select: "email" },
-  });
+export const signup = async ({ username, email, password }) => {
+  try {
+    const duplicatedAccount = await findOne({
+      model: UserModel,
+      filter: { email },
+      options: { select: "email" },
+    });
 
-  if (duplicatedAccount) {
-    throw ConflictException("Email Exists");
+    if (duplicatedAccount) {
+      throw ConflictException("Email already Exists");
+    }
+
+    const account = await createOne({
+      model: UserModel,
+      data: {
+        username,
+        email,
+        password,
+      },
+    });
+
+    return account;
+  } catch (error) {
+    console.log({ error });
+    throw error;
   }
-
-  const account = await createOne({
-    model: UserModel,
-    data: { email, password, username },
-  });
-
-  return account;
 };
 
 export const login = async ({ email, password }) => {
-  const account = await findOne({
-    model: UserModel,
-    filter: { email, password },
-    options: { select: "-password" },
-  });
+  try {
+    const account = await findOne({
+      model: UserModel,
+      filter: { email, password },
+      options: { select: "-password" },
+    });
 
-  if (!account) {
-    throw NotfoundException("Not Exist");
+    if (!account) {
+      throw NotfoundException("Not Exist");
+    }
+
+    return account;
+  } catch (error) {
+    console.log({ error });
+    throw error;
   }
-
-  return account;
 };
